cfd8bb1dfbca0d985332e109e35e4e121856d6b4
Sein Win Htut <niwang1616@gmail.com>
2025-07-31 11:34:03 +0700

    commit

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/test4.github.io/midterm-quotation-app/",
-  plugins: [react()],
-});
